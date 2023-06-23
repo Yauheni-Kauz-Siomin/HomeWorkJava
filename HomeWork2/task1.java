b3a23d81b0c351d345a1e5c3bcdb7971b65300e4
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-06-23 16:40:22 +0300

    add solving task#2.1

--- HomeWork2/task1.java
+++ HomeWork2/task1.java
@@ -2,9 +2,8 @@
 // Сформируйте часть WHERE этого запроса, используя StringBuilder. 
 // Данные для фильтрации приведены ниже в виде json строки.
 // Если значение null, то параметр не должен попадать в запрос.
-
-// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-// ===============================================================================================
+// Пример данной строки {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
 
 
 package HomeWork.HomeWorkJava.HomeWork2;
