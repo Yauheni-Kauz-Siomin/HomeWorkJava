7471928a7071fe8251697c6593bcbd54f2fe87f8
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-07-04 13:35:12 +0300

    add laptopStore

---
+++ HomeWork6/NoteBook.java
@@ -0,0 +1,63 @@
+
+package HomeWork.HomeWorkJava.HomeWork6;
+
+import java.util.*;
+
+public class NoteBook {
+    private String name; 
+    private Integer ram; 
+    private Integer hd; 
+    private String os; 
+    private String color; 
+
+    //сеттеры-геттеры
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setRam(Integer ram) {
+        this.ram = ram;
+    }
+    public Integer getRam() {
+        return ram;
+    }
+    public void setHd(Integer hd) {
+        this.hd = hd;
+    }
+    public Integer getHd() {
+        return hd;
+    }
+    public void setOs(String os) {
+        this.os = os;
+    }
+    public String getOs() {
+        return os;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    // конструктор
+    public NoteBook (String name, Integer ram, Integer hd, String os, String color){
+        this.name = name;
+        this.ram = ram;
+        this.hd = hd;
+        this.os = os;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return name + "; " +
+                "ОЗУ:" + ram +  "; " +
+                "Диск: " + hd + "; " +
+                "ОС: " + os + "; " +
+                "цвет:" + color + "; ";
+    }  
+
+}
